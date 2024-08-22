b0b82291265611c9e7ae3fbc658bed1eb5b3a43e
Sudhanshu Kumar <sudhanshuk.1024@gmail.com>
2024-08-22 15:30:54 +0530

    Finished writing Cat program

--- cat/src/main.rs
+++ cat/src/main.rs
@@ -1,3 +1,33 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, Read};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let mut path;
+
+    let file_paths = env::args();
+
+    for (index, path_string) in file_paths.enumerate() {
+        if index == 0 {
+            continue;
+        }
+        path = Path::new(&path_string);
+
+        match cat(path) {
+            Ok(s) => {
+                println!("\n{}: \n{}", path.file_name().unwrap().to_str().unwrap(), s);
+            }
+            Err(e) => println!("\nError: {}", e),
+        }
+    }
+}
+
+fn cat(path: &Path) -> io::Result<String> {
+    let mut f = File::open(path)?;
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
